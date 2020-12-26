26e74e1258a7d233f8b538fcefd9f9a959bd7c75
Dmytro Horpynchenko <d.horpynchenko@gmail.com>
2020-12-26 02:26:40 +0100

    update search request

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ import jsonpickle
 import os
 
 search_data = {
-    "category": 1,
+    # Mark for accom offers request
+    "offer_filter": 1,
+    # 1 - 1-Room-flats, 0 - Flatshares, 2 - Flats, 3 - Houses
+    "categories[]": [1],
     "city_id": 90,
-    "rent_type": 0,
     "noDeact": 1,
-    "sin": 2,
+    # 2 - No swap
     "exc": 2,
     "img_only": 1,
-    "rent_types[]": 0
+    # 0 - any , 1 - Short term, 2 - Long term, 3 - Overnight stay
+    "rent_types[]": [2]
 }
 
 url = "https://www.wg-gesucht.de"
